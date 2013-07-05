6f3210ba5018b1cb37ba4f5c5b6a6278c44c1605
lichee <luleier@gmail.com>
2013-07-05 18:55:02 +0800

    add some basic functions

--- mycsv.py
+++ mycsv.py
@@ -2,11 +2,23 @@ import csv
 
 def main():
 	"""docstring for fname"""
-	print "fad"
-	pass
-#csvfile=file()
+	print "CSV file Analyzer"
 
+	try:
+		csvfile = open("test.csv",'rb')
+	except IOError:
+		print "Open CSV file error"
+	csvdata = csv.reader(csvfile)
+	
+	i = 0
+	for row in csvdata:
+		if i == 10:
+			break
+		i = i + 1
+		print row
 
+	csvfile.close()
+	print "ended"
 
 
 if __name__ == "__main__":
